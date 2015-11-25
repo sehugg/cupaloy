51d373bccfac62c044d650b46b78aad7993c2e74
Steven Hugg <hugg@fasterlight.com>
2015-11-24 20:54:44 -0500

    OSXMountInfo

--- mount.py
+++ mount.py
@@ -1,21 +1,65 @@
 #!/usr/bin/python
 
 import subprocess
+from sys import platform as _platform
+from plistlib import readPlistFromString
 
-def mountInfoForPath(path):
-  # TODO: what if no findmnt?
-  return (
-    subprocess.check_output(["findmnt","-no","uuid","-T",path]).strip(),
-    subprocess.check_output(["findmnt","-no","target","-T",path]).strip()
-  )
+###
 
-def mountLocationForUUID(uuid):
-  return subprocess.check_output(["findmnt","-no","target","UUID="+uuid]).strip()
+class LinuxMountInfo:
+
+  def forPath(self, path):
+    # TODO: what if no findmnt?
+    return (
+      subprocess.check_output(["findmnt","-no","uuid","-T",path]).strip(),
+      subprocess.check_output(["findmnt","-no","target","-T",path]).strip()
+    )
+
+  def locationForUUID(self, uuid):
+    return subprocess.check_output(["findmnt","-no","target","UUID="+uuid]).strip()
+
+###
+
+class OSXMountInfo:
+
+  def __init__(self):
+    xml = subprocess.check_output(["diskutil","list","-plist"])
+    plist = readPlistFromString(xml)
+    mounts = []
+    for disk in plist['AllDisksAndPartitions']:
+      for part in disk['Partitions']:
+        mp = part.get('MountPoint')
+        if mp:
+          mounts.append((part.get('VolumeUUID'), mp))
+    self.mounts = mounts
+
+  def forPath(self, path):
+    best = None
+    for m in self.mounts:
+      if path.startswith(m[1]):
+        if not best or len(m[1]) > len(best[1]):
+          best = m
+    return best
+
+  def locationForUUID(self, uuid):
+    for m in self.mounts:
+      if m[0] == uuid:
+        return m[1]
+    return None
+
+###
+
+if _platform == "darwin":
+  mountInfo = OSXMountInfo()
+elif _platform == "win32":
+  mountInfo = WindowsMountInfo()
+else:
+  mountInfo = LinuxMountInfo()
 
 ###
 
 if __name__ == '__main__':
-  print mountInfoForPath("/")
-  print mountInfoForPath("a06997a7-9a7a-4395-9aa2-8630f3eb13b2")
-  print mountLocationForUUID("2012-07-03-20-55-41-00")
+  print mountInfo.forPath("/")
+  print mountInfo.forPath("a06997a7-9a7a-4395-9aa2-8630f3eb13b2")
+  print mountInfo.locationForUUID("2012-07-03-20-55-41-00")
   
